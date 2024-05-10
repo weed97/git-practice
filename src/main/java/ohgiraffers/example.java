1ca22fe753b4ecf7d71a7bbb5f8c7321d21e49e6
weed97 <weed9935@gmail.com>
2024-05-10 10:56:22 +0900

    first comit

---
+++ src/main/java/ohgiraffers/example.java
@@ -0,0 +1,11 @@
+package ohgiraffers;
+
+public class example {
+    public static void main(String[] args) {
+
+        System.out.print("Hello World");
+        System.out.println("한글은 써저요");
+
+
+    }
+}
